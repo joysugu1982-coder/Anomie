e04ef83fcfe49a93059e3a45df09c07320e1a6eb
Sugu <joysugu1982@gmail.com>
2025-12-06 08:55:41 +0700

    Fix OpenGraph image renderer and remove font fs usage

--- components/opengraph-image.tsx
+++ components/opengraph-image.tsx
@@ -1,56 +1,46 @@
 import { ImageResponse } from "next/og";
 import LogoIcon from "./icons/logo";
-import path from "path";
-import { promises as fs } from "fs";
 
-export type Props = {
-  title?: string;
-};
+export const runtime = "edge";
 
-export default async function OpengraphImage(
-  props?: Props
-): Promise<ImageResponse> {
-  const { title } = {
-    ...{
-      title: process.env.SITE_NAME,
-    },
-    ...props,
-  };
-
-  // Load ALL fonts
-  const gafitonFont = await fs.readFile(
-    path.join(process.cwd(), "public/fonts/Gafiton.woff")
-  );
-
-  const montserratRegular = await fs.readFile(
-    path.join(process.cwd(), "public/fonts/Montserrat-Regular.woff")
-  );
-
-  const montserratSemi = await fs.readFile(
-    path.join(process.cwd(), "public/fonts/Montserrat-SemiBold.woff")
-  );
-
-  const nobileRegular = await fs.readFile(
-    path.join(process.cwd(), "public/fonts/Nobile-Regular.woff")
-  );
-
-  const nobileBold = await fs.readFile(
-    path.join(process.cwd(), "public/fonts/Nobile-Bold.woff")
-  );
+export default async function OpengraphImage() {
+  const title = process.env.SITE_NAME || "My Store";
 
   return new ImageResponse(
     (
-      <div tw="flex h-full w-full flex-col items-center justify-center bg-black">
-
-        <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
+      <div
+        style={{
+          display: "flex",
+          height: "100%",
+          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "black",
+          color: "white",
+          fontFamily: "sans-serif",
+        }}
+      >
+        {/* FIXED: MUST ADD display:flex HERE */}
+        <div
+          style={{
+            display: "flex",
+            height: 160,
+            width: 160,
+            border: "1px solid #666",
+            borderRadius: "24px",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <LogoIcon width="64" height="58" fill="white" />
         </div>
 
-        {/* Title using Nobile */}
+        {/* FIXED: P TAG MUST HAVE display:flex TOO */}
         <p
-          tw="mt-12 text-white"
           style={{
-            fontFamily: "Nobile",
+            display: "flex",
+            marginTop: 48,
             fontSize: 72,
             fontWeight: 700,
           }}
@@ -58,11 +48,10 @@ export default async function OpengraphImage(
           {title}
         </p>
 
-        {/* Subtitle using Montserrat */}
         <p
-          tw="mt-4 text-white"
           style={{
-            fontFamily: "Montserrat",
+            display: "flex",
+            marginTop: 16,
             fontSize: 32,
             fontWeight: 400,
           }}
@@ -74,38 +63,6 @@ export default async function OpengraphImage(
     {
       width: 1200,
       height: 630,
-      fonts: [
-        {
-          name: "Gafiton",
-          data: gafitonFont,
-          weight: 700,
-          style: "normal",
-        },
-        {
-          name: "Montserrat",
-          data: montserratRegular,
-          weight: 400,
-          style: "normal",
-        },
-        {
-          name: "Montserrat",
-          data: montserratSemi,
-          weight: 600,
-          style: "normal",
-        },
-        {
-          name: "Nobile",
-          data: nobileRegular,
-          weight: 400,
-          style: "normal",
-        },
-        {
-          name: "Nobile",
-          data: nobileBold,
-          weight: 700,
-          style: "normal",
-        },
-      ],
     }
   );
 }
